290873a6ca2a7adc65f2be7f4e7931bb7a0057cb
Ruchir Mandora <ruchirm702@gmail.com>
2024-08-21 23:27:57 +0530

    Initial commit with base project setup

---
+++ src/main/java/dev/ruchir/notes_lake/NotesLakeApplication.java
@@ -0,0 +1,13 @@
+package dev.ruchir.notes_lake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesLakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesLakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ruchir/notes_lake/NotesLakeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruchir.notes_lake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesLakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
